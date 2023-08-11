33dfcafa49ad5d698d3c3b2ff30257133ff35efe
moonpiedumplings <moonpiedumplings2@gmail.com>
2023-08-11 11:08:48 -0700

    Built site for gh-pages

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-    pkgs = import <nixpkgs> {};
-    quarto = pkgs.callPackage ./env/quarto.nix {};
-in
-    pkgs.mkShell {
-        packages = with pkgs; [ 
-            quarto
-            ];
-    }
