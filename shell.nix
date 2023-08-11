7a5c463ee04833e2d90f7102627a285e46d32241
moonpiedumplings <moonpiedumplings2@gmail.com>
2023-08-11 10:21:42 -0700

    started on docs, moved stuff around

---
+++ shell.nix
@@ -0,0 +1,9 @@
+let
+    pkgs = import <nixpkgs> {};
+    quarto = pkgs.callPackage ./env/quarto.nix {};
+in
+    pkgs.mkShell {
+        packages = with pkgs; [ 
+            quarto
+            ];
+    }
