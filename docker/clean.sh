d4df0540c646b84bc2b968d9cc28a4e694e742c1
moonpiedumplings <moonpiedumplings2@gmail.com>
2023-08-15 13:55:05 -0700

    work on kata containers

--- docker/clean.sh
+++ docker/clean.sh
@@ -2,5 +2,5 @@
 # This script cleans up the docker install of ranch.
 # Warning: It NUKES EVERYTHING
 # run with sudo
-rm -rfi rancher/
+rm -rf rancher/
 docker container rm docker-rancher-manager-1
